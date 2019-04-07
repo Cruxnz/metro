1f5a352c8211ea8f50bd99ba33ae06ce7da496e3
Dan <danielkermode@hotmail.co.nz>
2019-04-07 23:04:57 +1200

    Begin db scaffold

--- backend/MetroServer/app/__init__.py
+++ backend/MetroServer/app/__init__.py
@@ -1,3 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://metro:password@localhost/metro_db'
+db = SQLAlchemy(app)
 from app import views
+from app import models
--- backend/MetroServer/app/views.py
+++ backend/MetroServer/app/views.py
@@ -1,5 +1,15 @@
 from app import app
 
+# Routes
 @app.route('/')
-def home():
-   return "hello world!"
+def root():
+  return app.send_static_file('index.html')
+
+@app.route('/<path:path>')
+def static_proxy(path):
+  # send_static_file will guess the correct MIME type
+  return app.send_static_file(path)
+
+
+if __name__ == '__main__':
+  app.run()
